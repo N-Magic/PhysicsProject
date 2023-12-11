529a8159024989ee2c5eab272e51f3a22a47d64d
N-Magic <94937494+N-Magic@users.noreply.github.com>
2023-12-11 13:48:13 -0600

    Update Projectile.py

--- Projectile.py
+++ Projectile.py
@@ -1,41 +1,6 @@
 import sys
 import math
 
-def render(time, distance, height, vertSpeed, horSpeed):
-    frameTime = time/50
-    heightSegment = height/40
-    widthSegment = distance/40
-    if widthSegment > heightSegment:
-        segment = widthSegment
-    else:
-        segment = heightSegment
-    divisor = frameTime
-    rendering = 0
-    horizontalPosition = 0
-    verticalPosition = 0
-    frame = []
-    while rendering < time:
-        time += frameTime
-        horizontalPosition += (horSpeed/frameTime)
-        verticalPosition += (vertSpeed/frameTime)
-        vertSpeed += (-(9.8/100))
-        for i in range(50):
-            currentSlice = []
-            fromBase = int(verticalPosition/heightSegment)
-            for i in range(49 - int(fromBase)):
-                currentSlice.append(" ")
-            currentSlice.append("#")
-            print("one done")
-            for i in range(int(fromBase)):
-                currentSlice.append(" ")
-            frame.append(currentSlice)
-    print(frame)
-
-
-
-
-
-
 try:
     velocity = int(sys.argv[1])
     print("Object Velocity is: " + str(velocity))
@@ -57,9 +22,6 @@ try:
         if verticalPosition > maxHeight:
             maxHeight = verticalPosition
     print("The object went " + str(round(horizontalPosition, 3)) + " meters forwards and " + str(round(maxHeight,3)) + " meters at highest over the course of " + str(round(time,3)) + " seconds.")
-    
-
 except:
     print("Make sure to format comamand as pypy3 Projectile.py *velocity* *angle from ground*")
 
-render(time, horizontalPosition, maxHeight, initialVertSpeed, horSpeed)
